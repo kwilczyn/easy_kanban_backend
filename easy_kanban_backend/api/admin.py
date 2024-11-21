8e72c9267c306229594c405693b11eb687dc157a
Krzysztof Wilczyński <kwilczyn89@icloud.com>
2024-11-21 19:05:29 +0100

    feat: update admin view for Board model

--- easy_kanban_backend/api/admin.py
+++ easy_kanban_backend/api/admin.py
@@ -8,8 +8,16 @@ class UserAdmin(admin.ModelAdmin):
         model = User
         filter_horizontal = ('boards',)
 
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'created_at', 'updated_at')
+    list_filter = ['users']
+    class Meta:
+        model = Board
+    
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
-admin.site.register(Board)
+admin.site.register(Board, BoardAdmin)
 admin.site.register(List)
 admin.site.register(Task)
